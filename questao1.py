8c2c091424116cc38fe090d21d179f7bcd43a2e5
soyEldvd <jdca.dvd@alu.ufc.br>
2022-12-02 18:26:27 -0300

    Histograma/CDF/Media e variância de uma geração de 4 a 12

---
+++ questao1.py
@@ -0,0 +1,42 @@
+import matplotlib as plt
+import numpy as np
+from time import time
+#entrando com os valores
+a = 1029
+c = 3333
+x = time()
+m = 2**64
+v1 = np.zeros(10000)#definindo um vetor de tamanho
+
+for i in range(10000):
+    func = ((a * x + c) % m)
+    x = func
+    v1[i] = (4 + (func / (m -1)) * 8) #dessa forma, faço com que os valores que estavam de 0 a 1 fiquem entre o intervalo de k1 a k2
+
+#plotando o histograma da distribuição:
+x = v1
+plt.hist(x)
+plt.show()
+
+#plotando a cdf
+count, binscount = np.histogram(x)
+pdf = count / sum(count)
+cdf = np.cumsum(pdf)
+plt.plot(binscount[1:], cdf)
+plt.show()
+
+#calculando a media
+media = 0
+for i in range (10000):
+    media = media + v1[i]
+
+media /= 10000
+
+#calculando a variancia 
+variancia = 0
+for i in range(10000):
+    variancia = variancia + (v1[i] - media) ** 2
+
+variancia /= 10000
+
+print(f"media {media} \n variancia {variancia}")
